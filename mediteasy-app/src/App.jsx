cab3bb3f5c1d7fe3ea6ab417f839b04977f41a00
OlivierF47 <olivier.forabosco05@gmail.com>
2025-10-01 16:53:05 +0200

    merge

--- mediteasy-app/src/App.jsx
+++ mediteasy-app/src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar.jsx';
 import GongSelector from './components/GongSelector.jsx';
-import AmbientSoundSelector from './components/AmbientSelector.jsx';
+import AudioUploader from './components/FileUpload.jsx';import AmbientSoundSelector from './components/AmbientSelector.jsx';
+import MeditationTimerController from './components/MeditationTimerController.jsx';
+import Sakura from './components/Sakura.jsx';
 
 const App = () => {
   return (
