7e3553742e1ecb032b4b141048aa6d1f92f61ff2
OlivierF47 <olivier.forabosco05@gmail.com>
2025-10-01 10:11:02 +0200

    initialisation de l'uploader

--- mediteasy-app/src/App.jsx
+++ mediteasy-app/src/App.jsx
@@ -1,9 +1,10 @@
 import GongSelector from './components/GongSelector.jsx';
-
+import AudioUploader from './components/FileUpload.jsx';
 const App = () => {
   return (
     <div>
       <h1>Notre site</h1>
+      <AudioUploader/>
       <GongSelector />
     </div>
   );
